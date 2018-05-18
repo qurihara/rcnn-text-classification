bd9ec7c7fb653330bd7fddf6f638d1242634116c
qurihara <qurihara@unryu.org>
2018-05-18 19:13:07 +0900

    ok

--- index.py
+++ index.py
@@ -18,33 +18,14 @@ from chainer import cuda
 import numpy as np
 from model import LetterClassifyer
 
-def argument():
-    parser = argparse.ArgumentParser()
-#    parser.add_argument('mode')
-#    parser.add_argument('file')
-    parser.add_argument('--embed', default=200, type=int)
-    parser.add_argument('--vocab', default=3000, type=int)
-    parser.add_argument('--hidden', default=1000, type=int)
-    parser.add_argument('--epoch', default=100, type=int)
-    parser.add_argument('--model', default="model")
-    parser.add_argument('--classes', default=2, type=int)
-    parser.add_argument('--use-gpu', action='store_true', default=False)
-    parser.add_argument('--unchain', action='store_true', default=False)
-    args = parser.parse_args()
-    return args
 
 def eval(name):
-    args = argument()
-    if args.use_gpu:
-        xp = cuda.cupy
-        cuda.get_device(0).use()
-    else:
-        xp = np
-    vocab = Vocabulary.load_from_file("%s.vocab" % args.model)
-    m = LetterClassifyer(args.vocab, args.embed, args.hidden, args.classes)
-    chainer.serializers.load_hdf5("%s.hdf5" % args.model, m)
-    if args.use_gpu:
-        m.to_gpu()
+    xp = cuda.cupy
+    cuda.get_device(0).use()
+    vocab = Vocabulary.load_from_file("model.vocab")
+    m = LetterClassifyer(3000, 200, 1000, 2)
+    chainer.serializers.load_hdf5("model.hdf5", m)
+    m.to_gpu()
     x_batch = [letter_list_text(name)]
     output = forward(x_batch, None, m, False, vocab, xp)
     return np.argmax(output.data)

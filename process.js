c01ffaca7e29685d77565f8d3c412b89a318399b
qurihara <qurihara@unryu.org>
2018-05-18 16:45:03 +0900

    ok

--- process.js
+++ process.js
@@ -1,3 +1,6 @@
+//青空文庫からテキストをダウンロードし， nkf -w --overwrite sample.txt でutfにした．
+//次に全角スペースと「と」をテキストエディタで削除した．
+
 var fs = require("fs");
 var readline = require("readline");
 
@@ -5,9 +8,12 @@ var stream = fs.createReadStream(process.argv[2], "utf8");
 
 var reader = readline.createInterface({ input: stream });
 reader.on("line", (data) => {
-  var d1 = data.trim();
+  var d1 = data.trim().replace(/《.*?》/g,"");
   var sents = d1.split("。");
   for (var i=0;i<sents.length;i++){
-    console.log(sents[i]);
+    if (sents[i].length <= 1) continue;
+    var line = sents[i]+","+process.argv[4]+"\n";
+    fs.appendFile(process.argv[3],line);
+    console.log(line);
   }
 });
---
+++ process.sh
@@ -0,0 +1,2 @@
+node process.js data/hashire_merosu.txt data.txt 0
+node process.js data/rashomon.txt data.txt 1
